bc17f05da603f6503e9196f3f4848ab7305b3122
vagrant <vagrant@lighthouse-labs>
2014-05-28 03:18:46 +0000

    event class validated

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,27 @@
 class Event < ActiveRecord::Base
 
+  validates :title, 
+            presence: true
+
+  validates :location, 
+            presence: true
+
+  validates :guest_number, 
+            presence: true
+
+  validates :attire, 
+            presence: true
+
+  validates :event_date, 
+            presence: true
+
+  validates :start_time, 
+            presence: true
+
+  validates :end_time, 
+            presence: true
+
+  validates :food_bio, 
+            presence: true
+
 end
